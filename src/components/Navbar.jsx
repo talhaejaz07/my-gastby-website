6a4e928442591c6efef660be7ff1e953a9d73dc0
talhaejaz07 <talhaejaz@outlook.com>
2022-03-20 02:13:43 +0500

    Starting slowly

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>My Work</li>
+        <li>My Blog</li>
+      </ul>
+    </nav>
+  )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "../global.css"
+
+import Navbar from "../components/Navbar"
+
+export default function Home() {
+  return <Navbar />
+}
