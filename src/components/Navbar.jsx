ea909accdbe106f263d44718a5a8accf482bfe1e
talhaejaz07 <talhaejaz@outlook.com>
2022-03-20 16:11:20 +0500

    image plugin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,51 @@
 import React from "react"
+import styled from "styled-components"
+import banner from "../assets/images/Banner.webp"
 
 export default function Navbar() {
   return (
-    <nav>
+    <Wrapper>
       <ul>
-        <li>My Work</li>
-        <li>My Blog</li>
+        <li>
+          <a href="#">My Work</a>
+        </li>
+        <li>
+          <a href="#">My Blog</a>
+        </li>
       </ul>
-    </nav>
+      <img src={banner} alt="banner" />
+      <ul>
+        <li>
+          <a href="#">About Me</a>
+        </li>
+        <li>
+          <a href="#">Contact Me</a>
+        </li>
+      </ul>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.nav`
+  display: grid;
+  grid-template-columns: auto auto auto;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    height: 150px;
+  }
+
+  ul {
+    display: flex;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--txt-clr);
+  }
+
+  li {
+    list-style-type: none;
+  }
+`
